554d42ba11266af546f3c7c884e7944ff8194cde
wilrakov <wilrakov@proton.me>
2026-06-30 18:58:12 +0200

    add(gfx): add some geometrics support and C array support for flushing it on the screen, it is fast so i like it

--- src/gfx/gfx.cpp
+++ src/gfx/gfx.cpp
@@ -2,16 +2,27 @@
 #include <LovyanGFX.hpp>
 #include <algorithm>
 
+void Renderer::drawRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color) {
+    commands.emplace_back(RECTANGLE, x, y, w, h, color, nullptr, Rect{x, y, w, h});
+}
+
 void Renderer::fillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color) {
-    commands.push_back({RECTANGLE, x, y, w, h, color, nullptr, {x, y, w, h}});
+    commands.emplace_back(FILLED_RECTANGLE, x, y, w, h, color, nullptr, Rect{x, y, w, h});
+}
+
+void Renderer::drawRoundRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t radius, uint16_t color) {
+    commands.emplace_back(ROUND_RECTANGLE, x, y, w, h, radius, color, Rect{x, y, w, h});
+}
+
+void Renderer::fillRoundRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t radius, uint16_t color) {
+    commands.emplace_back(FILLED_ROUND_RECTANGLE, x, y, w, h, radius, color, Rect{x, y, w, h});
 }
 
 void Renderer::fillCircle(uint16_t cx, uint16_t cy, uint16_t r, uint16_t color) {
     uint16_t d = r * 2;
     uint16_t rx = (cx > r) ? cx - r : 0;
     uint16_t ry = (cy > r) ? cy - r : 0;
-    GfxCommand command = {CIRCLE, cx, cy, r, 0, color, nullptr, {rx, ry, d, d}};
-    commands.push_back(command);
+    commands.emplace_back(CIRCLE, cx, cy, r, 0, color, Rect{rx, ry, d, d});
 }
 
 void Renderer::drawLine(uint16_t startx, uint16_t starty, uint16_t endx, uint16_t endy, uint16_t color) {
@@ -19,16 +30,24 @@ void Renderer::drawLine(uint16_t startx, uint16_t starty, uint16_t endx, uint16_
     uint16_t y = (starty < endy) ? starty : endy;
     uint16_t w = std::max(startx, endx) - x + 1;
     uint16_t h = std::max(starty, endy) - y + 1;
-    GfxCommand command = {LINE, startx, starty, endx, endy, color, nullptr, {x, y, w, h}};
-    commands.push_back(command);
+    commands.emplace_back(LINE, startx, starty, endx, endy, color, Rect{x, y, w, h});
 }
 
 void Renderer::drawText(const char *text, uint16_t x, uint16_t y, uint16_t font_size, uint16_t color) {
     lcd->setTextSize(font_size);
     uint16_t h = lcd->fontHeight();
     uint16_t w = lcd->textWidth(text);
-    GfxCommand command = {TEXT, x, y, w, font_size, color, text, {x, y, w, h}};
-    commands.push_back(command);
+    commands.emplace_back(TEXT, x, y, w, font_size, color, text, Rect{x, y, w, h});
+}
+
+void Renderer::drawBitmap(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t *bitmap) {
+    commands.emplace_back(BITMAP, x, y, w, h, bitmap, Rect{x, y, w, h});
+}
+
+void Renderer::drawScaledBitmap(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t scale, uint16_t color, const uint16_t *bitmap) {
+    uint16_t rectw = static_cast<uint16_t>(w * scale);
+    uint16_t recth = static_cast<uint16_t>(h * scale);
+    commands.emplace_back(BITMAP_SCALED, x, y, w, h, scale, color, bitmap, Rect{x, y, rectw, recth});
 }
 
 void Renderer::render() {
@@ -76,8 +95,18 @@ void Renderer::render() {
                 int16_t rel_y = cmd.y1 - rect.y;
                 switch(cmd.type) {
                     case RECTANGLE:
+                        sprite.drawRect(rel_x, rel_y, cmd.x2, cmd.y2, cmd.color);
+                        break;
+                    case FILLED_RECTANGLE:
                         sprite.fillRect(rel_x, rel_y, cmd.x2, cmd.y2, cmd.color);
                         break;
+                    case ROUND_RECTANGLE:
+                        sprite.drawRoundRect(rel_x, rel_y, cmd.x2, cmd.y2, cmd.radius, cmd.color);
+                        break;
+                    case FILLED_ROUND_RECTANGLE:
+                        sprite.fillRoundRect(rel_x, rel_y, cmd.x2, cmd.y2, cmd.radius, cmd.color);
+                        break;
+
                     case CIRCLE:
                         sprite.fillCircle(rel_x, rel_y, cmd.x2, cmd.color);
                         break;
@@ -88,6 +117,20 @@ void Renderer::render() {
                         sprite.setTextColor(cmd.color);
                         sprite.setTextSize(cmd.y2);
                         sprite.drawString(cmd.text, rel_x, rel_y);
+                        break;
+                    case BITMAP:
+                        sprite.pushImage(rel_x, rel_y, cmd.x2, cmd.y2, (const uint16_t*)cmd.bitmap);
+                        break;
+                    case BITMAP_SCALED:
+                        sprite.pushImageRotateZoom(
+                            (float)rel_x, (float)rel_y,
+                            0.0f, 0.0f,
+                            0.0f,
+                            cmd.size, cmd.size,
+                            cmd.x2, cmd.y2,
+                            (const uint16_t*)cmd.bitmap,
+                            cmd.color
+                        );
                         break;
                 }
             }
--- src/gfx/gfx.hpp
+++ src/gfx/gfx.hpp
@@ -6,9 +6,15 @@
 
 enum CommandType {
     RECTANGLE,
+    FILLED_RECTANGLE,
+    ROUND_RECTANGLE,
+    FILLED_ROUND_RECTANGLE,
+
     CIRCLE,
     LINE,
     TEXT,
+    BITMAP,
+    BITMAP_SCALED,
 };
 
 struct Rect {
@@ -42,8 +48,28 @@ struct GfxCommand {
     CommandType type;
     uint16_t x1, y1, x2, y2;
     uint16_t color;
-    const char *text;
+    uint16_t size;
+    union {
+        const uint16_t *bitmap;
+        const char *text;
+        uint16_t radius;
+    };
     Rect bounds;
+
+    GfxCommand(CommandType t, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color, Rect b)
+        : type(t), x1(x1), y1(y1), x2(x2), y2(y2), color(color), radius(0), bounds(b) {}
+
+    GfxCommand(CommandType t, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t radius, uint16_t color, Rect b)
+        : type(t), x1(x1), y1(y1), x2(x2), y2(y2), color(color), radius(radius), bounds(b) {}
+
+    GfxCommand(CommandType t, uint16_t x, uint16_t y, uint16_t font_w, uint16_t font_size, uint16_t color, const char* text, Rect b)
+        : type{t}, x1{x}, y1{y}, x2{font_w}, y2{font_size}, color{color}, text{text}, bounds{b} {}
+
+    GfxCommand(CommandType t, uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t* bitmap, Rect b)
+        : type{t}, x1{x}, y1{y}, x2{w}, y2{h}, color{0}, bitmap{bitmap}, bounds{b} {}
+
+    GfxCommand(CommandType t, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t scale, uint16_t color, const uint16_t* bitmap, Rect b)
+        : type{t}, x1{x}, y1{y}, x2{w}, y2{h}, size{scale}, color{color}, bitmap{bitmap}, bounds{b} {}
 };
 
 class Renderer {
@@ -54,13 +80,20 @@ private:
 public:
     Renderer(LGFX_Device *lcd) : lcd{lcd} {}
 
+    void drawRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
     void fillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+    void drawRoundRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t radius, uint16_t color);
+    void fillRoundRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t radius, uint16_t color);
     
     void fillCircle(uint16_t cx, uint16_t cy, uint16_t r, uint16_t color);
 
     void drawLine(uint16_t startx, uint16_t starty, uint16_t endx, uint16_t endy, uint16_t color);
 
     void drawText(const char *text, uint16_t x, uint16_t y, uint16_t font_size, uint16_t color);
+
+    void drawBitmap(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t *bitmap);
+    void drawScaledBitmap(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t scale, uint16_t color, const uint16_t *bitmap);
+
 
     void render();
 };
